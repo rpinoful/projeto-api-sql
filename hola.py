a1aaad0a7e7c834c0d8caad6876ba443f355c946
Tu Nombre <rapsful@gmail.com>
2026-05-23 17:54:42 -0300

    ending the day with if main testing
    and printing json name files

--- hola.py
+++ hola.py
@@ -2,6 +2,8 @@ import requests
 from pprint import pprint
 import json 
 import os
+import glob
+import pandas as pd
 
 # api request with the content save in memory
 try:
@@ -18,9 +20,23 @@ size_process :int = 10
 
 for i in range(0,len(products),size_process):
     chunk = products[i:i + size_process] # productos de 10 en 10 
-    nome_arquivo = f"data/products_{i//size_process}+1.json"
+    nome_arquivo = f"data/products_{i//size_process+1}.json"
 
     with open(nome_arquivo, 'w',encoding='utf-8') as f:
         json.dump(chunk,f,indent=4,ensure_ascii=False)
 
+
+
+def reading_jsonfiles_append(folder: str) -> pd.DataFrame:
+    files = glob.glob(os.path.join(folder,"products*.json"))
+    for file in files:
+        print(file)
+
+
+
+
+
+if __name__ == "__main__":
+    folder = "data"
+    df_append = reading_jsonfiles_append(folder)
 
