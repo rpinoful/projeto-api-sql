9510cd9ec3749152850ebde4b967f018fa2bcc55
Tu Nombre <rapsful@gmail.com>
2026-05-30 11:28:01 -0300

    ending append function and to starting to building transform dataframe

--- hola.py
+++ hola.py
@@ -5,38 +5,55 @@ import os
 import glob
 import pandas as pd
 
-# api request with the content save in memory
-try:
-    url = 'https://dummyjson.com/products'
-    response= requests.get(url)
-    data = response.json()
+
+
+
+def reading_jsonfiles_append(products:dict,folder: str) -> pd.DataFrame:
+
+    # processing files 
+    for i in range(0,len(products),size_process):
+        chunk = products[i:i + size_process] # productos de 10 en 10 
+        nome_arquivo = f"data/products_{i//size_process+1}.json" # creating json files from information before readed
+        # writing json file information
+        with open(nome_arquivo, 'w',encoding='utf-8') as f:
+            json.dump(chunk,f,indent=4,ensure_ascii=False)
+
     
-except Exception as e:
-    print(f" Error {e}")
-
-os.makedirs("data", exist_ok=True)
-products :dict= data['products']
-size_process :int = 10
-
-for i in range(0,len(products),size_process):
-    chunk = products[i:i + size_process] # productos de 10 en 10 
-    nome_arquivo = f"data/products_{i//size_process+1}.json"
-
-    with open(nome_arquivo, 'w',encoding='utf-8') as f:
-        json.dump(chunk,f,indent=4,ensure_ascii=False)
+    files = glob.glob(os.path.join(folder,"products*.json")) # list with all json files from a specific path
+    dataframes = [pd.read_json(file) for file in files]
+    df_concat = pd.concat(dataframes,ignore_index=True)
+    
+    return df_concat
 
 
 
-def reading_jsonfiles_append(folder: str) -> pd.DataFrame:
-    files = glob.glob(os.path.join(folder,"products*.json"))
-    for file in files:
-        print(file)
+
+
+
 
 
 
 
 
 if __name__ == "__main__":
+    try:
+        url = 'https://dummyjson.com/products'
+        response= requests.get(url)
+        data = response.json()
+        
+    except Exception as e:
+        print(f" Error {e}")
+
+    # creating data folder if doesnt exist
+    os.makedirs("data", exist_ok=True)
+    # taking only products from json response
+    products :dict= data['products']
+    # process size 
+    size_process :int = 10
+
+
     folder = "data"
-    df_append = reading_jsonfiles_append(folder)
+    df_append = reading_jsonfiles_append(products,folder)
+    print(df_append.columns)
+
 
